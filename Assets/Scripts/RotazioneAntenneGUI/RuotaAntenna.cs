1d3ea1eacee539642aefe661ec96f14a195acf08
Alessio Perugini <alessioper.98@gmail.com>
2018-01-27 15:35:29 +0100

    Rotazione antenna. Script che consente di ruotare alla pressione del pulsante dx/sx l'oggetto.

---
+++ Assets/Scripts/RotazioneAntenneGUI/RuotaAntenna.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RuotaAntenna : MonoBehaviour
+{
+    public Button destra, sinistra;
+    public float velocita;
+    public float movimento;
+    public GameObject canvas;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (movimento == 1)
+            transform.Rotate(0, velocita * Time.deltaTime, 0, Space.World);
+        if (movimento == 2)
+            transform.Rotate(0, -velocita * Time.deltaTime, 0, Space.World);
+    }
+
+    public void Fermo()
+    {
+        movimento = 0;
+    }
+    public void MuoviSinistra()
+    {
+        movimento = 2;
+    }
+
+    public void MuoviDestra()
+    {
+        movimento = 1;
+    }
+
+    void OnMouseDown()
+    {
+        canvas.gameObject.SetActive(true);
+        Debug.Log("onclick");
+    }
+    public void ApriCanvas()
+    {
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void ChiudiCanvas()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+}
